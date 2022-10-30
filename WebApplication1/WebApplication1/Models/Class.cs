6dd565bc36d406690100d2647610076bb1e7369c
Phuong772001 <phuongnvph15343@fpt.edu.vn>
2022-10-31 01:51:29 +0700

    vfdw

--- WebApplication1/WebApplication1/Models/Class.cs
+++ WebApplication1/WebApplication1/Models/Class.cs
@@ -2,8 +2,6 @@
 {
     public class Class
     {
-        public int teen { get; set; }
-        public int teen1 { get; set; }
-        public int teen2 { get; set; }
+        public int Type { get; set; }
     }
 }
